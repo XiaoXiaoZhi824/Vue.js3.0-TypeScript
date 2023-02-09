d7f8ff8fe90f3266432a54e360a58ec018d6adbe
张志颖 <1093598026@qq.com>
2023-02-09 15:12:08 +0800

    创建项目

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <h1>Hello World!!!</h1>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style lang="less" scoped></style>
