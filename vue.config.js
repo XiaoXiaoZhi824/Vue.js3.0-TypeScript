be590bfc656c7ddd74dfd960d19a1b18cbcd62e3
张志颖 <1093598026@qq.com>
2023-02-09 15:39:54 +0800

    vue.config.js-配置别名

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src'))
+      .set('assets', '@/assets')
+      .set('components', '@/components')
+      .set('views', '@/views')
+  }
 })
